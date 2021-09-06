979390fb8a27db6e387bc33c50319d8697ac6f6d
HeYue <yue.he@intel.com>
2021-09-06 13:42:44 +0800

    drm/i915: Sysfs interface to get GFX shmem usage
    There is a requirement of a new interface for achieving below
    functionalities:
    
    Need to provide Client based detailed information about the
    distribution of Graphics memory
    Need to provide an interface which can provide info about the
    sharing of Graphics buffers between the clients.
    The client based interface would also aid in debugging of
    memory usage/consumption by each client & debug memleak
    related issues.
    
    With this new interface,
    
    In case of memleak scenarios, we can easily zero in on the
    culprit client which is unexpectedly holding on the Graphics
    buffers for an inordinate amount of time.
    We can get an estimate of the instantaneous memory footprint
    of every Graphics client.
    We can now trace all the processes sharing a particular
    Graphics buffer.
    By means of this patch we try to provide a sysfs interface
    to achieve the mentioned functionalities.
    
    There are two files created in sysfs:
    'i915_gem_meminfo' will provide summary of the graphics
    resources used by each graphics client.
    'i915_gem_objinfo' will provide detailed view of each object
    created by individual clients.
    
    Rebased for kernel v4.19 1A (Fan Yugang)
    
    Removes unuseful shmem_inode_info->info_lock per Jeremy's optimization.
    Removes unuseful object info for each pid for performance optimization.
    Adds CONFIG_DRM_I915_MEMTRACK which depends on
    CONFIG_DRM_I915_CAPTURE_ERROR to pass kernel config test.
    
    Rebased for kernel v5.4 (He, Yue) (Wan, Shuang)
    
    Rebased for kernel v5.10 (He, Yue)
    
    Rebased for kernel v5.15.74 (rajucm)
    
    Tracked-On: OAM-99250
    Change-Id: Ib89cd2eaf6e1fc61311b43ebe54dbdc78554821b
    Signed-off-by: Sourab Gupta sourab.gupta@intel.com
    Sgined-off-by: Deepak S deepak.s@intel.com
    Signed-off-by: Hu Beiyuan beiyuan.hu@intel.com
    Signed-off-by: Mingwei Wang mingwei.wang@intel.com
    Signed-off-by: Harish Krupo harish.krupo.kps@intel.com
    Signed-off-by: Jeremy Compostella jeremy.compostella@intel.com
    Signed-off-by: Yugang Fan yugang.fan@intel.com
    Signed-off-by: Wan Shuang <shuang.wan@intel.com>
    Signed-off-by: HeYue <yue.he@intel.com>
    Signed-off-by: rajucm <raju.mallikarjun.chegaraddi@intel.com>

--- drivers/gpu/drm/i915/gem/i915_gem_object_types.h
+++ drivers/gpu/drm/i915/gem/i915_gem_object_types.h
@@ -490,6 +490,9 @@ struct drm_i915_gem_object {
 	 *   critical, i.e userspace is free to race against itself.
 	 */
 	unsigned int cache_dirty:1;
+#if IS_ENABLED(CONFIG_DRM_I915_MEMTRACK)
+	unsigned int has_backing_pages : 1;
+#endif
 
 	/* @is_dpt: Object houses a display page table (DPT) */
 	unsigned int is_dpt:1;
@@ -664,6 +667,9 @@ struct drm_i915_gem_object {
 
 		void *gvt_info;
 	};
+#if IS_ENABLED(CONFIG_DRM_I915_MEMTRACK)
+	struct list_head pid_info;
+#endif
 };
 
 static inline struct drm_i915_gem_object *

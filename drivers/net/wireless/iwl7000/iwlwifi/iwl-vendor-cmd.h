db0bd7be4339a650ed2949be9b9d017d11982454
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2021-10-08 10:08:48 +0530

    iwl7000: Add driver and firmware version query support
    Adding vendor command support for querying driver & firmware
    version.
    
    Tracked-On: OAM-94267
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-vendor-cmd.h
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-vendor-cmd.h
@@ -112,6 +112,8 @@
  * @IWL_MVM_VENDOR_CMD_GET_LINK_INFO: Get link information.
  *	This is needed for RFIm user app
  * @IWL_MVM_VENDOR_CMD_LINK_INFO_CHANGED_EVENT: Link information is changed
+ * @IWL_MVM_VENDOR_CMD_GET_DRV_VERSION: Driver version
+ * @IWL_MVM_VENDOR_CMD_GET_FW_VERSION: Firmware version
  */
 
 enum iwl_mvm_vendor_cmd {
@@ -170,6 +172,8 @@ enum iwl_mvm_vendor_cmd {
 	IWL_MVM_VENDOR_CMD_RFIM_SET_CNVI_MASTER			= 0x37,
 	IWL_MVM_VENDOR_CMD_GET_LINK_INFO			= 0x38,
 	IWL_MVM_VENDOR_CMD_LINK_INFO_CHANGED_EVENT		= 0x39,
+	IWL_MVM_VENDOR_CMD_GET_FW_VERSION                       = 0x41,
+	IWL_MVM_VENDOR_CMD_GET_DRV_VERSION                      = 0x42,
 };
 
 /**
@@ -806,6 +810,8 @@ enum iwl_vendor_auth_akm_mode {
  * @IWL_MVM_VENDOR_ATTR_RFIM_CHAIN_B_DESENSE: chain b desense values
  * @IWL_MVM_VENDOR_ATTR_RFIM_DDR_SNR_THRESHOLD: SNR threshold for RSSI based
  *	DDR RFIM.
+ * @IWL_MVM_VENDOR_ATTR_DRV_VER: string attribute. Driver version
+ * @IWL_MVM_VENDOR_ATTR_FW_VER: string attribute. Firmware version
  *
  * @NUM_IWL_MVM_VENDOR_ATTR: number of vendor attributes
  * @MAX_IWL_MVM_VENDOR_ATTR: highest vendor attribute number
@@ -929,6 +935,8 @@ enum iwl_mvm_vendor_attr {
 	IWL_MVM_VENDOR_ATTR_RFIM_CHAIN_A_DESENSE		= 0x7e,
 	IWL_MVM_VENDOR_ATTR_RFIM_CHAIN_B_DESENSE		= 0x7f,
 	IWL_MVM_VENDOR_ATTR_RFIM_DDR_SNR_THRESHOLD		= 0x80,
+	IWL_MVM_VENDOR_ATTR_FW_VER                              = 0x81,
+	IWL_MVM_VENDOR_ATTR_DRV_VER                             = 0x82,
 
 	NUM_IWL_MVM_VENDOR_ATTR,
 	MAX_IWL_MVM_VENDOR_ATTR = NUM_IWL_MVM_VENDOR_ATTR - 1,

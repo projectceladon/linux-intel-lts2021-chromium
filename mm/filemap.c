398303b894bbb07963250408ed87f5a120020a28
Takaya Saeki <takayas@chromium.org>
2024-06-20 15:23:51 +0000

    BACKPORT: FROMLIST: filemap: add trace events for get_pages, map_pages, and fault
    To allow precise tracking of page caches accessed, add new tracepoints
    that trigger when a process actually accesses them.
    
    The ureadahead program used by ChromeOS traces the disk access of
    programs as they start up at boot up. It uses mincore(2) or the
    'mm_filemap_add_to_page_cache' trace event to accomplish this. It stores
    this information in a "pack" file and on subsequent boots, it will read
    the pack file and call readahead(2) on the information so that disk
    storage can be loaded into RAM before the applications actually need it.
    
    A problem we see is that due to the kernel's readahead algorithm that
    can aggressively pull in more data than needed (to try and accomplish
    the same goal) and this data is also recorded. The end result is that
    the pack file contains a lot of pages on disk that are never actually
    used. Calling readahead(2) on these unused pages can slow down the
    system boot up times.
    
    To solve this, add 3 new trace events, get_pages, map_pages, and fault.
    These will be used to trace the pages are not only pulled in from disk,
    but are actually used by the application. Only those pages will be
    stored in the pack file, and this helps out the performance of boot up.
    
    With the combination of these 3 new trace events and
    mm_filemap_add_to_page_cache, we observed a reduction in the pack file
    by 7.3% - 20% on ChromeOS varying by device.
    
    Signed-off-by: Takaya Saeki <takayas@chromium.org>
    Reviewed-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Reviewed-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    (am from https://patchwork.kernel.org/patch/13761691/)
    
    Conflicts:
      mm/filemap.c
        an argument for trace_mm_filemap_fault is changed according to an
        old equivalent name.
    
    UPSTREAM-TASK=b:349539302
    BUG=b:328731130, b:336686836
    TEST=confirmed that new tracepoints work with trace-cmd
    
    Change-Id: I2400e7e04fa4ca870cf5e04d1f38ec8a17a4fe6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5790328
    Tested-by: Takaya Saeki <takayas@chromium.org>
    Commit-Queue: Takaya Saeki <takayas@chromium.org>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -2576,6 +2576,7 @@ retry:
 			goto err;
 	}
 
+	trace_mm_filemap_get_pages(mapping, index, last_index);
 	return 0;
 err:
 	if (err < 0)
@@ -3066,6 +3067,8 @@ vm_fault_t filemap_fault(struct vm_fault *vmf)
 	if (unlikely(offset >= max_off))
 		return VM_FAULT_SIGBUS;
 
+	trace_mm_filemap_fault(mapping, offset);
+
 	/*
 	 * Do we have something in the page cache already?
 	 */
@@ -3351,6 +3354,7 @@ unlock:
 		put_page(head);
 	} while ((head = next_map_page(mapping, &xas, end_pgoff)) != NULL);
 	pte_unmap_unlock(vmf->pte, vmf->ptl);
+	trace_mm_filemap_map_pages(mapping, start_pgoff, end_pgoff);
 out:
 	rcu_read_unlock();
 	WRITE_ONCE(file->f_ra.mmap_miss, mmap_miss);
